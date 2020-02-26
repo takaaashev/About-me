414db80c8d08be33adebed1b5f44067fafeb9918
takaaashev <37807843+takaaashev@users.noreply.github.com>
2020-02-26 16:52:46 +0500

    Добавление DOM элемента
    Показывает номер телефона при нажатии на "показать телефон"

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function buttonClick(button1){
+    var access =document.getElementById("button1");
+    var code = access.innerHTML;
+    access.innerHTML = '+79090978882';
+    // code = '+79090978882';
+    // alert (code);
+  }
